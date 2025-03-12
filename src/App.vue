f560c83ba83db6371fe8ee7d938f4ad3894baa8b
Gabriel Paiao <costapaiao@hotmail.com>
2025-03-12 17:10:34 -0300

    ✨ feat: Create App.vue screen to show data from OpenWeather API

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,31 @@
   <div class="weather-app">
     <h1>Weather App/Dashboard</h1>
     <div class="search-box">
-      <input
-      type="text"
-      placeholder="Enter city name"
-      v-model="city"
-      @keyup.enter="fecthWeather"
-      />
+      <input type="text" placeholder="Enter city name" v-model="city" @keyup.enter="fecthWeather" />
       <button @click="fetchWeather">Search</button>
     </div>
-    <div class="weather-footer"></div>
+    <div v-if="Weather" class="weather-info">
+      <h2>{{ weather.name }}, {{ weather.sys.country }}</h2>
+      <p>{{ weather.weather[0].description }}</p>
+      <p>Temperature: {{ weather.main.temp }}ºC</p>
+      <p>Humidity: {{ weather.main.humidity }}%</p>
+      <p>Wind Speed: {{ weather.wind.speed }} m/s</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Weather from './components/weather/Weather.vue';
 
+export default {
+  data() {
+    return {
+      city: '',
+      weather: null,
+    };
+  },
+}
 </script>
 
 <style>
