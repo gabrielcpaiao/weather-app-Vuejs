3eb0f1336d628ce0cad90e31eaf571e8acb893ef
Gabriel Paiao <costapaiao@hotmail.com>
2025-03-13 17:27:39 -0300

    :sparkles: Finalizing the integration with API Open Weather Map

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
 
 <script>
 import axios from 'axios';
-import Weather from './components/weather/Weather.vue';
+// import Weather from './components/weather/Weather.vue';
 
 export default {
   data() {
@@ -26,10 +26,48 @@ export default {
       weather: null,
     };
   },
-}
+  methods: {
+    async fetchWeather() {
+      try {
+        const API_KEY = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid={API_KEY}&units=metric'
+        const response = await axios.get(
+          'https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${API_KEY}&units=metric'
+        );
+        this.weather = response.data;
+      } catch (error) {
+        console.log('Error fetching weather data: ', error);
+        alert('City not found. Please try again.');
+      }
+    },
+  },
+};
 </script>
 
 <style>
+.weather-app {
+  text-align: center;
+  padding: 20px;
+}
+
+.search-box {
+  margin: 20px 0;
+}
+
+input {
+  padding: 10px;
+  font-size: 16px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  margin-left: 10px;
+}
+
+.weather-info {
+  margin-top: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
